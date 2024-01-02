8454e1dd2dd798380398e83c77e9955463d162e6
‘josh <kingorijosphat@yahoo.com>
2024-01-02 16:58:33 -0500

    This is our first repository containing a bash script

---
+++ patch-management.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# We are going to focus on ubuntu distros 
+# apt ==> the package manager we are going use to manage 
+# packages in our ubuntu server 
+
+# Command to check that all the packages are available in our system 
+
+sudo apt-get update 
+
+# we are now going to upgrade all the packages 
+
+sudo apt-get upgrade -y 
+
